b135d188bb0cacfa241a03ee9d47c3a6a10ce61b
aima kareem <amikareem921@gmail.com>
2025-03-09 16:21:01 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+{
+let myname=("Aima_kareem");
+let age=22;
+const MYNAME="AimaKareem";
+console.log(myname);
+console.log(age);
+console.log("My name is:",MYNAME);
+}
+
+{
+let name,age,degree,religion;
+name="Aima";
+age=25;
+degree="Bs";
+religion="Islam";
+
+console.log("name is:",name,"Age:",age,"degree:",degree,"Religion:",religion);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof degree);
+console.log(typeof religion);
+
+}
+// practice
+let keyboard={
+    productname:"keyboard",
+    rating:4.5,
+    scale:1000,
+    price:270,
+    return:true,
+    color:"black",
+};
+console.log(keyboard);
