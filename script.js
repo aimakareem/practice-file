01c5567cbfb06db88d301388652fe1d255c48d05
aima kareem <amikareem921@gmail.com>
2025-03-10 17:02:08 +0500

    operators

--- script.js
+++ script.js
@@ -31,3 +31,29 @@ let keyboard={
     color:"black",
 };
 console.log(keyboard);
+
+// arithematic operators
+let a=46;
+let b=53;
+console.log("a+b=",a+b); 
+console.log("a-b=",a-b); 
+console.log("a*b=",a*b); 
+console.log("a/b=",a/b); 
+console.log("a%b=",a%b); 
+console.log("a**b=",a**b); 
+console.log("value of a",a++);
+console.log("value of a",a++);
+// assignment operator
+let c=5;
+let d=4;
+let e=4;
+let f=4;
+let g=4;
+console.log(c+=2);
+console.log(d-=2);
+console.log(e*=2);
+console.log(f%=2);
+console.log(f**=2);
+
+
+
